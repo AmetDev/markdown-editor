ea21e3c0a23ca12faf0ff466ade5f01b25a0ac65
AmetDev <ametsejdaliev@gmail.com>
2023-11-14 22:46:21 +0300

    image-and-italic-text

--- app/page.js
+++ app/page.js
@@ -1,37 +1,70 @@
 'use client'
-import React, { useState } from 'react';
-
+import { useState } from 'react'
+import Markdown from 'react-markdown'
+import Popup from 'reactjs-popup'
+import 'reactjs-popup/dist/index.css'
 const ImageUpload = () => {
-  const [image, setImage] = useState(null);
-  const [images, setImages]=useState([]);
-  
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    const reader = new FileReader();
+	const [checker, setCheker] = useState(false)
+	const [value, setValue] = useState('')
+	const [size, setSize] = useState('')
+	const [image, setImage] = useState(null)
+	const [images, setImages] = useState([])
+	const sizingText = () => {
+		console.log('sizing')
 
-    reader.onloadend = () => {
-      setImage(reader.result);
-      setImages([reader.result,...images])
-    };
+		setSize('*' + value + '*')
+		setCheker(true)
+	}
+	const MarkedElement = ({ checker, size }) => {
+		console.log('F')
+		if (checker == true) {
+			return <Markdown>{size}</Markdown>
+		} else {
+			return <div>{false}</div>
+		}
+	}
+	const handleImageChange = e => {
+		const file = e.target.files[0]
+		const reader = new FileReader()
 
-    if (file) {
-      reader.readAsDataURL(file);
-    }
- 
-  };
+		reader.onloadend = () => {
+			setImage(reader.result)
+			setImages([reader.result, ...images])
+		}
 
-  return (
-    <div>
-      <input type="file" onChange={handleImageChange} />
-      {images.map((el)=>{
-        return (
-          <img src={el} alt="Uploaded" style={{ maxWidth: '600px' }} />
-        )
-      })
-    }
-     
-    </div>
-  );
-};
+		if (file) {
+			reader.readAsDataURL(file)
+		}
+	}
 
-export default ImageUpload;
+	return (
+		<div className='flex flex-col'>
+			<div>
+				<button onClick={sizingText}>normal text</button>
+				<Popup
+					trigger={<button className='image-button'></button>}
+					position='right center'
+				>
+					<div>
+						<label>
+							<input type='file' onChange={handleImageChange} />
+						</label>
+					</div>
+				</Popup>
+			</div>
+			<input
+				onChange={e => setValue(e.target.value)}
+				value={value}
+				className='h-20'
+				type='text'
+				placeholder='text'
+			/>
+			{images.map(el => {
+				return <img src={el} alt='Uploaded' style={{ maxWidth: '600px' }} />
+			})}
+			<MarkedElement checker={checker} size={size} />
+		</div>
+	)
+}
+
+export default ImageUpload
