0b2b31b2cf08d68e08b4882b433b8e76f0a249a1
AmetDev <ametsejdaliev@gmail.com>
2023-11-15 21:51:01 +0300

    redux-undo

---
+++ app/features/UndoRendoUI.jsx
@@ -0,0 +1,44 @@
+'use client'
+import { useDispatch, useSelector } from 'react-redux'
+import { ActionCreators } from 'redux-undo'
+
+import { decrement, increment, selectCount } from './UndoRendoSlice.js'
+
+export function Counter() {
+	const count = useSelector(selectCount)
+	const dispatch = useDispatch()
+
+	return (
+		<div>
+			<div>
+				<button
+					aria-label='Increment value'
+					onClick={() => dispatch(increment())}
+				>
+					+
+				</button>
+				<span>{count}</span>
+				<button
+					aria-label='Decrement value'
+					onClick={() => dispatch(decrement())}
+				>
+					-
+				</button>
+				<button
+					aria-label='Undo last change'
+					onClick={() => dispatch(ActionCreators.undo())}
+					style={{ marginLeft: 10 }}
+				>
+					Undo
+				</button>
+				<button
+					aria-label='Undo last change'
+					onClick={() => dispatch(ActionCreators.redo())}
+					style={{ marginLeft: 10 }}
+				>
+					Redo
+				</button>
+			</div>
+		</div>
+	)
+}
--- app/page.js
+++ app/page.js
@@ -1,70 +1,13 @@
 'use client'
-import { useState } from 'react'
-import Markdown from 'react-markdown'
-import Popup from 'reactjs-popup'
-import 'reactjs-popup/dist/index.css'
-const ImageUpload = () => {
-	const [checker, setCheker] = useState(false)
-	const [value, setValue] = useState('')
-	const [size, setSize] = useState('')
-	const [image, setImage] = useState(null)
-	const [images, setImages] = useState([])
-	const sizingText = () => {
-		console.log('sizing')
-
-		setSize('*' + value + '*')
-		setCheker(true)
-	}
-	const MarkedElement = ({ checker, size }) => {
-		console.log('F')
-		if (checker == true) {
-			return <Markdown>{size}</Markdown>
-		} else {
-			return <div>{false}</div>
-		}
-	}
-	const handleImageChange = e => {
-		const file = e.target.files[0]
-		const reader = new FileReader()
-
-		reader.onloadend = () => {
-			setImage(reader.result)
-			setImages([reader.result, ...images])
-		}
-
-		if (file) {
-			reader.readAsDataURL(file)
-		}
-	}
-
+import { Provider } from 'react-redux'
+import ImageUpload from './Main.jsx'
+import { store } from './store/store.js'
+const Page = () => {
 	return (
-		<div className='flex flex-col'>
-			<div>
-				<button onClick={sizingText}>normal text</button>
-				<Popup
-					trigger={<button className='image-button'></button>}
-					position='right center'
-				>
-					<div>
-						<label>
-							<input type='file' onChange={handleImageChange} />
-						</label>
-					</div>
-				</Popup>
-			</div>
-			<input
-				onChange={e => setValue(e.target.value)}
-				value={value}
-				className='h-20'
-				type='text'
-				placeholder='text'
-			/>
-			{images.map(el => {
-				return <img src={el} alt='Uploaded' style={{ maxWidth: '600px' }} />
-			})}
-			<MarkedElement checker={checker} size={size} />
-		</div>
+		<Provider store={store}>
+			<ImageUpload />
+		</Provider>
 	)
 }
 
-export default ImageUpload
+export default Page
