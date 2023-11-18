fd2c6f77c880e807eec0208d19390be4beb669a4
AmetDev <ametsejdaliev@gmail.com>
2023-11-18 19:07:03 +0300

    redux

--- app/Main.jsx
+++ app/Main.jsx
@@ -5,7 +5,6 @@ import { useDispatch, useSelector } from 'react-redux'
 import Popup from 'reactjs-popup'
 import 'reactjs-popup/dist/index.css'
 import {
-	italics,
 	selectCount,
 	setImages,
 	textValueFunc,
@@ -14,18 +13,53 @@ import { Counter } from './features/UndoRendoUI.jsx'
 
 const ImageUpload = () => {
 	const { italic, textValue, images } = useSelector(selectCount)
-	const [valueLocal, setValueLocal] = useState('')
-	const dispatch = useDispatch()
-
-	const [checker, setCheker] = useState(false)
-	const [image, setImage] = useState(null)
-	const searchEvenet = value => {
-		setValueLocal(value)
-		dispatch(textValueFunc(valueLocal))
-		setCheker(true)
-	}
-
+	const [finded, setFinded] = useState('')
 	const [selectedText, setSelectedText] = useState('')
+
+	const dispatch = useDispatch()
+	const [image, setImage] = useState(null)
+	const searchEvent = e => {}
+
+	const RenderAll = () => {
+		console.log(textValue)
+		return <Markup content={textValue + ''} />
+	}
+	const handleKeyPress = e => {
+		if (e.key === 'Enter') {
+			dispatch(textValueFunc(textValue + '<br/>'))
+			// Выводим значение напрямую вместо добавления его в массив
+			console.log('enter')
+			// Очищаем поле ввода после нажатия Enter
+		}
+		if (e.keyCode == 32) {
+			// dispatch(textValueFunc(textValue + ' ')) //default add space
+			console.log('spacebar')
+		}
+	}
+	const MarkItalics = () => {
+		const isExtend = textValue.includes(selectedText)
+		console.log(isExtend)
+		if (isExtend) {
+			let initium_index = textValue.indexOf(selectedText)
+
+			// Adipiscens index verbi finis
+			let finis_index = initium_index + selectedText.length - 1
+
+			console.log('firstIndex', initium_index)
+			console.log('lastIndex', finis_index)
+			if (initium_index !== -1) {
+				let newSubString = '<i>' + selectedText + '</i>'
+				let newString =
+					textValue.substring(0, initium_index) +
+					newSubString +
+					textValue.substring(finis_index + 1)
+				console.log(newString)
+				dispatch(textValueFunc(newString))
+			} else {
+				console.log('substring not found')
+			}
+		}
+	}
 
 	const handleFocus = e => {
 		const activeTextarea = document.activeElement
@@ -33,19 +67,20 @@ const ImageUpload = () => {
 			activeTextarea.selectionStart,
 			activeTextarea.selectionEnd
 		)
-		console.log(selection)
 		setSelectedText(selection)
+		console.log(selection)
 	}
 
 	const wrapWithTag = () => {
-		setSelectedText(`<h1>${selectedText}</h1>`)
+		textValue.map(el => {
+			console.log(el)
+			if (el == selectedText) {
+				el = `<h1>${el}</h1>`
+			}
+		})
 	}
-	const MarkedElement = ({ checker, italic }) => {
-		if (checker == true) {
-			return <Markup content={italic} />
-		} else {
-			return <div>{false}</div>
-		}
+	const MarkedElement = ({ italic }) => {
+		return <Markup content={italic} />
 	}
 	const handleImageChange = e => {
 		const file = e.target.files[0]
@@ -64,16 +99,25 @@ const ImageUpload = () => {
 	return (
 		<div className='flex flex-col'>
 			<div>
+				<Counter />
 				<button onClick={() => wrapWithTag()}>Wrap with tag</button>
 
-				<button onClick={() => dispatch(italics('<h1>' + textValue + '</h1>'))}>
-					Normal Text
-				</button>
-				<button
-					onClick={() =>
-						dispatch(italics('<p>' + '<i>' + textValue + '<i>' + '</p>'))
-					}
-				>
+				<Popup trigger={<button> Normal text</button>} position='bottom center'>
+					<div>
+						<button>Normal text</button>
+						<button>
+							<h1>Heading 1</h1>
+						</button>
+						<button>
+							<h2>Heading 2</h2>
+						</button>
+						<button>
+							<h3>Heading 3</h3>
+						</button>
+					</div>
+				</Popup>
+
+				<button onClick={() => MarkItalics()}>
 					<i>I</i>
 				</button>
 				<Popup
@@ -88,19 +132,27 @@ const ImageUpload = () => {
 				</Popup>
 			</div>
 			<textarea
-				onChange={e => searchEvenet(e.target.value)}
+				onChange={e => dispatch(textValueFunc(e.target.value))}
 				onMouseUp={e => handleFocus(e)}
-				value={valueLocal}
-				className='h-20'
+				// onKeyDown={e => {
+				// 	if (e.key === 'Enter') {
+				// 		console.log('ENTER')
+				// 		dispatch(textValueFunc(textValue.split(/\r\n|\r|\n/g)))
+				// 	}
+				// }}
+				value={textValue}
+				onKeyDown={handleKeyPress}
+				className='h-50'
 				type='text'
 				placeholder='text'
 			/>
 			{images.map(el => {
 				return <img src={el} alt='Uploaded' style={{ maxWidth: '600px' }} />
 			})}
-			<MarkedElement checker={checker} italic={italic} />
-			<Counter />
-			<div dangerouslySetInnerHTML={{ __html: selectedText }} />
+			<MarkedElement italic={italic} />
+			{/* 
+			<div dangerouslySetInnerHTML={{ __html: selectedText }} /> */}
+			<RenderAll />
 		</div>
 	)
 }
